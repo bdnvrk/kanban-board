1bb54faf920ddf2ab9d1b0dbeac9d14eb814caf0
Marcin Trepa <marcint339@wp.pl>
2018-04-25 21:44:40 +0200

    Adding list with static name

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import map from 'lodash/fp/map';
+import { Button } from 'react-bootstrap';
+import { addNewList } from '../../actions';
 import List from '../List';
 import Header from '../Header';
 
-const Board = ({ lists }) => (
-  <div className="container">
-    <Header />
-    <div className="row">
-      {map(list => {
-        return (
-          <List key={list.id} name={list.name} />
-        );
-      })(lists)}   
+const Board = ({ lists, addNewList }) => {
+  return (
+    <div className="container">
+      <Header />
+      <Button bsStyle="primary" onClick={() => addNewList("placeholder")}>Dodaj nową listę</Button>
+      <div className="row">
+        {map(list => {
+          return (
+            <List key={list.id} name={list.name} />
+          );
+        })(lists)}   
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 const mapStateToProps = (state) => ({
   lists: state.lists,
 });
 
-export default connect(mapStateToProps)(Board);
+const mapDispatchToProps = {
+  addNewList,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Board);
