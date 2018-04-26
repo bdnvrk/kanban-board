a09a06cefa54290354f82138b2c946326ed8550f
bdnvrk <bednarek.tomasz@live.com>
2018-04-26 18:35:38 +0200

    render fix

--- src/components/AppComponent/index.js
+++ src/components/AppComponent/index.js
@@ -7,29 +7,34 @@ class AppComponent extends Component {
   constructor() {
     super()
     this.state = {
-      connectionStatus: 'notConnected'
+      isConnected: false
     }
   }
   firebaseDatabaseConnectionTest() {
     /* 
       Connection checking function
+      TODO: remove in future
     */
     const fireDatabase = firebase.database();
     fireDatabase.ref('/testValue')
       .once('value')
       .then((snapshot) => {
-        this.setState({
-          connectionStatus: snapshot.val()
-        })
+        if (snapshot.val() !== '') {
+          this.setState({
+            isConnected: true
+          })
+        }
       })
   }
 
   render() {
-    this.firebaseDatabaseConnectionTest()
-
+    if (!this.state.isConnected) {
+      this.firebaseDatabaseConnectionTest()
+    }
+    
     return (
       <div className="App">
-        {this.state.connectionStatus}
+        {this.state.isConnected ? 'connected' : 'not connected'}
       </div>
     );
   }
